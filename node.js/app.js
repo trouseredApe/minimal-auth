08e5325650fe1829cc2df686b8e250d6376333ed
trouseredape <pratik.pandey+c9@gmail.com>
2015-12-08 23:27:13 +0000

    basic-auth

--- node.js/app.js
+++ node.js/app.js
@@ -1,13 +1,11 @@
-var express = require("express"); 
-
+var express = require("express");
 var bodyParser = require("body-parser"); 
+
 var app = express();
- 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
  
 var routes = require("./routes/routes.js")(app);
- 
 var server = app.listen(process.env.PORT, function () {
     console.log("Listening on port %s...", server.address().port);
 });
--- node.js/routes/routes.js
+++ node.js/routes/routes.js
@@ -1,27 +1,34 @@
+var auth = require("basic-auth");
+
 var appRouter = function(app) {
  app.get("/", function(req, res) {
-    
     res.send("it works!!!");
-   
  });
 
- app.post("/auth", function(req, res) {
-    var username = req.body.username;
-    var pw = req.body.password;
-    var e0 = req.body.e0
+ app.get("/auth", function(req, res) {
+    console.log(req.headers.authorization);
+    var user = auth(req);
+    var username = user.name;
+    
+    console.log(user);
     
     var mockUser = {}
     mockUser.email =  "admin@aghost.net";
     mockUser.nickname =  "admin";
     if(username == "E000090101") {
       mockUser.username = "E000090101"
+      mockUser.user_id = 1;
     }
 
     if(username == "E000090102") {
       mockUser.username = "E000090101"
+      mockUser.user_id = 2;
     }
-    return res.send(mockUser);	
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.json(mockUser);	
  });
 
 } 
+
 module.exports = appRouter;
